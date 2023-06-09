4d189f0fa874554eac2551cf2f3b6c104cb595fb
Wishmi Tharuka <92284830+vishmitharuka57@users.noreply.github.com>
2023-06-09 13:39:40 +0530

    completed home page

---
+++ My Profile/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Vishmi Tharuka ","Frontend Developer","Freelancer","Web Designer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop: true
+})
